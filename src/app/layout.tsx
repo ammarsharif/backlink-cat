0e4ec9173c37d99673ab2b123a7b928157bf1672
Ammar Sharif <80442381+ammarsharif@users.noreply.github.com>
2026-05-09 16:14:19 +0500

    fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${poppins.variable} ${inter.variable}`}>
+    <html lang="en" className={`${poppins.variable} ${inter.variable}`} suppressHydrationWarning>
       <body className="antialiased">{children}</body>
     </html>
   );
